9647235ab8c217d1501f0dc582e94ac87a0ad230
maks-nurgazy <maksatbek.bolushov@gmail.com>
2020-02-22 00:25:29 +0600

    adding custom login page

--- src/main/java/fingerprint/manas/edu/kg/controller/LoginController.java
+++ src/main/java/fingerprint/manas/edu/kg/controller/LoginController.java
@@ -1,7 +1,14 @@
 package fingerprint.manas.edu.kg.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
 
-
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "login-logout/plain-login";
+    }
 
 }
