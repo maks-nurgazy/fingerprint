be75caae3f290887426d9bf15deeed79601b127b
maks-nurgazy <maksatbek.bolushov@gmail.com>
2020-02-22 00:55:25 +0600

    security packet changed

--- src/main/java/fingerprint/manas/edu/kg/security/SecurityConfig.java
+++ src/main/java/fingerprint/manas/edu/kg/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package fingerprint.manas.edu.kg.config;
+package fingerprint.manas.edu.kg.security;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
