1597faae92cd3451bdfc36c7a2aa932f71e1690b
maks-nurgazy <maksatbek.bolushov@gmail.com>
2020-03-10 14:10:57 +0600

    created develop branch

--- src/main/java/attendance/manas/edu/kg/controller/FingerprintController.java
+++ src/main/java/attendance/manas/edu/kg/controller/FingerprintController.java
@@ -1,15 +1,11 @@
 package attendance.manas.edu.kg.controller;
 
-import attendance.manas.edu.kg.entity.Student;
 import attendance.manas.edu.kg.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/students")
@@ -18,7 +14,7 @@ public class FingerprintController {
     StudentService studentService;
 
     @GetMapping("/{id}/add-fingerprint")
-    public String fingerprintForm(@PathVariable("id")String rollNumber) {
+    public String fingerprintForm(@PathVariable("id") String rollNumber) {
         return null;
     }
 
